4785fa14ebf4e6d74cef46f05e9e14f51a933cf2
andrewgough94 <andrewgough94@gmail.com>
2017-01-06 15:49:04 -0800

    Conflict training

--- src/Driver.java
+++ src/Driver.java
@@ -4,7 +4,7 @@
 public class Driver {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Heya tyler");
+        System.out.println("Heya paulo");
         System.out.println("Hey there guy");
     }
 }
